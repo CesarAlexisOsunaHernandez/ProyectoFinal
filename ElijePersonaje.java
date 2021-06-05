4ebda951cf1b2314a3b35fc7c480dab33bf218be
unknown <cesar.osuna23@uabc.edu.mx>
2021-06-05 14:16:19 -0700

    version 1.8

---
+++ ElijePersonaje.java
@@ -0,0 +1,72 @@
+import javax.swing.*; //Interfaz grafica
+import java.awt.Color; //Color
+import java.awt.event.*; //Boton
+import java.io.*; //File
+
+public class ElijePersonaje extends Pantalla{
+	private JLabel personaje, hom, mu;
+	private JButton hombre, mujer, siguiente;
+	private Personaje per = new Personaje();
+	private Audio audio = new Audio();
+	
+	public ElijePersonaje(){
+		super(300, 300);
+		
+		personaje = new JLabel("Haz tu personaje");
+		personaje.setBounds(80,10,300,30);
+		add(personaje);
+		
+		mu = new JLabel(new ImageIcon("IMAGENES/Paula.png"));
+		mu.setBounds(140,70,70,100);
+		mu.setVisible(false);
+		add(mu);
+		
+		hom = new JLabel(new ImageIcon("IMAGENES/Ness.png"));
+		hom.setBounds(140,70,70,110);
+		hom.setVisible(false);
+		add(hom);
+		
+		hombre = new JButton("Ni\u00f1o");
+		hombre.setBounds(30,80,80,30); //setBounds(x,y,width,height)
+		add(hombre);
+		hombre.addActionListener(this);
+		
+		mujer = new JButton("Ni\u00f1a");
+		mujer.setBounds(30,130,80,30); //setBounds(x,y,width,height)
+		add(mujer);
+		mujer.addActionListener(this);
+		
+		siguiente = new JButton("Siguiente");
+		siguiente.setBounds(30,200,100,30); //setBounds(x,y,width,height)
+		siguiente.setEnabled(false);
+		add(siguiente);
+		siguiente.addActionListener(this);
+		
+		audio.Musica("EligePersonaje");
+	}
+	
+	public void actionPerformed(ActionEvent c){
+		if(c.getSource() == hombre){
+			hom.setVisible(true);
+			mu.setVisible(false);
+			per.setGenero("hombre");
+			siguiente.setEnabled(true);
+		}
+		else if(c.getSource() == mujer){
+			hom.setVisible(false);
+			mu.setVisible(true);
+			per.setGenero("mujer");
+			siguiente.setEnabled(true);
+		}
+		else if(c.getSource() == siguiente){
+			Audio.detener();
+			Mundo n3 = new Mundo(per, false);
+			n3.setBounds(0,0,500,450);
+			n3.setVisible(true);
+			n3.setResizable(false);
+			n3.setLocationRelativeTo(null);
+			this.setVisible(false);
+		}
+	}
+	
+}
---
+++ Inicio.java
@@ -0,0 +1,84 @@
+import java.awt.Color; //Color
+import javax.swing.*; //Interfaz grafica
+import java.awt.event.*; //Boton ComboBox
+import javax.swing.event.*; //Checkbox
+
+public class Inicio extends Pantalla{
+	private JLabel titulo, logo, premio;
+	private JButton nuevoJuego, continuarJuego, salir;
+	private JComboBox cb;
+	private Serializadora ser = new Serializadora();
+	private Audio audio = new Audio();
+	private ManejarContinues MC = new ManejarContinues();
+	
+	public Inicio(Personaje per){
+		super(300, 300);
+		
+		premio = new JLabel(new ImageIcon("IMAGENES/Premio.png"));
+		premio.setBounds(20,40,50,59);
+		if(per.getJefe() == 8){
+			premio.setVisible(true);
+		}
+		else{
+			premio.setVisible(false);
+		}
+		add(premio);
+		
+		logo = new JLabel(new ImageIcon("IMAGENES/Icono.png"));
+		logo.setBounds(110,50,56,28);
+		add(logo);
+		
+		titulo = new JLabel("Heroe Matematico: Primaria");
+		titulo.setBounds(60,10,300,30);
+		titulo.setVisible(true);
+		add(titulo);
+		
+		nuevoJuego = new JButton("Nuevo juego");
+		nuevoJuego.setBounds(85,100,110,30);
+		add(nuevoJuego);
+		nuevoJuego.setVisible(true);
+		nuevoJuego.addActionListener(this);
+		
+		continuarJuego = new JButton("Continuar");
+		continuarJuego.setBounds(85,150,110,30);
+		continuarJuego.addActionListener(this);
+		add(continuarJuego);
+		
+		
+		salir = new JButton("Cerrar");
+		salir.setBounds(85,200,110,30);
+		salir.setVisible(true);
+		salir.addActionListener(this);
+		add(salir);
+		
+		
+		if(ser.leerObjeto(1) == null && ser.leerObjeto(2) == null && ser.leerObjeto(3) == null){
+			continuarJuego.setEnabled(false);
+		}
+		
+		audio.Musica("Inicio");
+	}
+	
+	public void actionPerformed(ActionEvent c){ //Botones
+		
+		if(c.getSource() == nuevoJuego){
+			Audio.detener();
+			ElijePersonaje n2 = new ElijePersonaje();
+			n2.setBounds(0,0,300,300);
+			n2.setVisible(true);
+			n2.setResizable(false);
+			n2.setLocationRelativeTo(null);
+			this.setVisible(false);
+		}
+		else if(c.getSource() == continuarJuego){
+			if(MC.elegirContinue() == JOptionPane.OK_OPTION){
+				this.setVisible(false);
+			}
+		}
+		else if(c.getSource() == salir){
+			Audio.detener();
+			System.exit(0);
+		}
+		
+	}
+}
